f1141a4917831ebfad925e959ab485bcc30972fa
Rory O'Connor <roconnor2@northwell.edu>
2022-01-06 18:10:07 +0000

    image plugin changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,12 +58,11 @@ module.exports = {
     },
 
     /**
-     * The following two plugins are required if you want to use Gatsby image
-     * See https://www.gatsbyjs.com/docs/gatsby-image/#setting-up-gatsby-image
-     * if you're curious about it.
+     * New gatsby-plugin-image plugins: https://www.gatsbyjs.com/docs/reference/release-notes/image-migration-guide/
      */
-    `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
 
     {
       // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
