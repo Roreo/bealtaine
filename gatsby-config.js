ec1c0355746fef13db512dd3b2e4d30379a9fa03
Rory O'Connor <roconnor2@northwell.edu>
2022-04-06 19:35:37 -0400

    sdflkjds

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,8 +66,26 @@ module.exports = {
     /**
      * New gatsby-plugin-image plugins: https://www.gatsbyjs.com/docs/reference/release-notes/image-migration-guide/
      */
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `dominantColor`,
+          quality: 50,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        },
+        failOnError: false,
+      },
+    },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-catch-links`,
